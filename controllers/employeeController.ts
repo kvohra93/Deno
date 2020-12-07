7f03436ce430050222a0cff3472f39d0e23e3951
Kunal <hackedin2@gmail.xcm>
2020-12-07 06:16:48 -0600

    Update employeeController.ts

--- controllers/employeeController.ts
+++ controllers/employeeController.ts
@@ -18,11 +18,22 @@ let employees: Employee[] = [
     name: "Mark Belman",
     designation: "Foreman",
     age: 40,
+  }, {
+    id: "4",
+    name: "Hank Moody",
+    designation: "Quality Control",
+    age: 39,
+  },
+  {
+    id: "4",
+    name: "Dwayne Jefferson",
+    designation: "Delivery Executive",
+    age: 38,
   },
 ];
 
-// @desc Get Products
-// @route Get /api/products
+// @desc Get Employee
+// @route Get /api/employees
 const getEmployees = async (request: any, response: any) => {
   await response.json({
     error: false,
@@ -30,89 +41,4 @@ const getEmployees = async (request: any, response: any) => {
   });
 };
 
-// @desc Get Single Employee
-// @route Get /api/employee/:id
-const getEmployee = async (request: any, response: any) => {
-  //console.log(request.params);
-  const employee: Employee | undefined = employees.find(
-    (p) => p.id === request.params.id
-  );
-  if (employee) {
-    await response.json({
-      error: false,
-      data: employee,
-    });
-  } else {
-    response.status = 404;
-    await response.json({
-      error: true,
-      message: "No product found",
-    });
-  }
-};
-
-// @desc Add product
-// @route Post /api/products
-const addEmployee = async (request: any, response: any) => {
-  //   console.log(await request.data);
-  if (!request.data) {
-    response.status = 400;
-    response.json({
-      error: true,
-      message: "No data",
-    });
-  } else {
-    const employee: Employee = await request.data;
-    employee.id = v4.generate();
-    employees.push(employee);
-    response.json({
-      error: false,
-      data: employee,
-    });
-  }
-};
-
-// @desc Update Product
-// @route PUT /api/products/:id
-const updateEmployee = async (request: any, response: any) => {
-  const id_param = await request.params.id;
-  const data = await request.data;
-  const employee: Employee | undefined = employees.find((p) => p.id === id_param);
-
-  if (employee) {
-    const updateData: {
-      name?: string;
-      designation?: string;
-      age?: number;
-    } = data;
-
-    employees = employees.map((p) =>
-      p.id === id_param ? { ...p, ...updateData } : p
-    );
-
-    response.json({
-      error: false,
-      data: employees,
-    });
-  } else {
-    response.status = 400;
-    response.json({
-      error: true,
-      message: "No data",
-    });
-  }
-};
-
-// @desc Delete Product
-// @route DELETE /api/product/:id
-const deleteEmployee = async (request: any, response: any) => {
-  const id_param = await request.params.id;
-  employees = employees.filter((p) => p.id !== id_param);
-  response.json({
-    error: false,
-    message: "Product Deleted",
-    data: employees,
-  });
-};
-
-export { getEmployees, getEmployee, addEmployee, updateEmployee, deleteEmployee };
+export { getEmployees};
